db826545eeb5dcbaf379043b34ae7f1d648ce5a0
vicho86 <viktor@bgomedia.net>
2015-01-15 15:24:54 +0200

    Homework Variables

---
+++ Solution2/Solution2/ConsoleApplication2/FloatDouble.cs
@@ -0,0 +1,24 @@
+﻿// Problem 2. Float or Double?
+// 
+//     Which of the following values can be assigned to a variable of type float and which to a variable of type double: 
+//     34.567839023, 12.345, 8923.1234857, 3456.091?
+//     Write a program to assign the numbers in variables and print them to ensure no precision is lost.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        float f1 = 12.345f;
+        float f2 = 3456.091f;
+        double d1 = 34.567839023;
+        double d2 = 8923.1234857;
+
+        Console.WriteLine(f1);
+        Console.WriteLine(f2);
+        Console.WriteLine(d1);
+        Console.WriteLine(d2);
+    }
+}
---
+++ Solution2/Solution2/ConsoleApplication4/Unicode.cs
@@ -0,0 +1,22 @@
+﻿// Problem 4. Unicode Character
+// 
+//     Declare a character variable and assign it with the symbol that has Unicode code 42 (decimal) using the \u00XX syntax, and then print it.
+// 
+// Hint: first, use the Windows Calculator to find the hexadecimal representation of 42. The output should be *.
+
+using System;
+
+class UnicodeCharacter
+
+{
+    static void Main()
+    {
+        int code = 42;
+                
+        char hex = '*';
+
+        Console.WriteLine((int)hex);
+        
+        Console.WriteLine((char)code);
+    }
+}
