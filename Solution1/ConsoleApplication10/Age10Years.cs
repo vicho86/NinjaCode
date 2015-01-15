bab67505570ef4ed7518540414d7b9058f9335e2
vicho86 <viktor@bgomedia.net>
2015-01-15 15:41:21 +0200

    Homework 1

---
+++ Solution1/ConsoleApplication10/Age10Years.cs
@@ -0,0 +1,18 @@
+﻿// Problem 15.* Age after 10 Years
+// Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+
+using System;
+
+class AgeIn10Years
+
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your age?");
+        
+        int age = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine( "Your age will be " + (age + 10) + " in 10 years!");
+    }
+        
+}
---
+++ Solution1/ConsoleApplication7/FirstLastName.cs
@@ -0,0 +1,14 @@
+﻿// Problem 7. Print First and Last Name
+// 
+//     Create console application that prints your first and last name, each at a separate line.
+
+using System;
+
+class PrintFirstLastName
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Fiki");
+        Console.WriteLine("Storaro");
+    }
+}
